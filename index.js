401f1150b8a38a8170d2f3db6d57eb38c5dac3ab
Civoremo <omerovic_n@hotmail.com>
2019-01-15 14:23:17 -0500

    initial projects commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js');
 
 const server = express();
+const db = knex(knexConfig.development);
 
 server.use(express.json());
 server.use(helmet());
